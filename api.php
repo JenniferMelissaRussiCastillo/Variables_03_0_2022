f1fc0b264016ff67728dec45007d7780d37d960b
JenniferMelissaRussiCastillo <jennifer.russi@hotmail.com>
2022-10-15 14:00:45 -0500

    Variables

--- api.php
+++ api.php
@@ -1,4 +1,37 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-echo "Soy el servisor variabless".$_SERVER['HTTP_HOST']." gracias por conectarme :)";
+
+$primerNombre="Jennifer";
+$nombreCompleto="Jennifer Melissa Russi Castillo";
+$edad=32;
+$altura= 1.55;
+$soyAlumno= true;
+$pasaTiempo=["Ver peliculas","viajar"];
+$misDirecciones = new stdClass();
+$misDirecciones->principal= "Cigarras";
+$misDirecciones->secundario= "Cabecera";
+$misTelefonos= (object)[];
+ 
+
+echo $primerNombre;
+echo ("<br>"); 
+echo $nombreCompleto;
+echo ("<br>"); 
+echo $edad;
+echo ("<br>");
+echo $altura;
+echo ("<br>");
+echo ("Mi pasatiempo es: ".$pasaTiempo[1]);
+
+var_dump ($soyAlumno);
+echo ("<br>");
+
+
+echo $pasaTiempo[1];
+
+var_dump($pasaTiempo);
+var_dump($misTelefonos);
+var_dump($misDirecciones);
+echo ("<br>");
+echo "Soy el servisor variables   ".$_SERVER['HTTP_HOST']." gracias por conectarme :)";
 ?>
