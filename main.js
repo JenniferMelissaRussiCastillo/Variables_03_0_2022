b419a2d9839d60aaafd406298ae7fac96cef64a2
JenniferMelissaRussiCastillo <jennifer.russi@hotmail.com>
2022-10-17 11:05:47 -0500

    Variables

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", async(e)=>{
-    let peticion = await fetch("api.php");
+    let peticion = await fetch("https://jennifermelissarussicastillo.000webhostapp.com/Variables_03_0_2022/api.php");
     let data = await peticion.text();
     document.body.innerHTML =data;
 })
